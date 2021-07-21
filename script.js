b04e8ede3bb55c2ab5f89288cea2e6df9da1eeca
raythompsonwebdev <ray_thomp@hushmail.com>
2021-07-21 17:46:52 +0100

    added math random function and clientWidth property. alert used to signal winner of race.

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 const tortoiseOne = document.querySelector(".tortoise-one");
 const tortoiseTwo = document.querySelector(".tortoise-two");
-
+const raceArea = document.querySelector(".race-area");
 const goButton = document.querySelector(".race-go");
+let a = 0;
+let b = 0;
+var distancetoFinishA = raceArea.clientWidth - 10;
+
+function mouseClick() {
+  const myVar = setInterval(() => {
+    tortoiseOne.style.transform = `translateX(${a}px)`;
+    tortoiseTwo.style.transform = `translateX(${b}px)`;
+
+    b += Math.random() * 10;
+    a += Math.random() * 10;
+
+    if (distancetoFinishA <= a) {
+      clearInterval(myVar);
+      alert("Tortiose One has won!!!");
+    }
+    if (distancetoFinishA <= b) {
+      clearInterval(myVar);
+      alert("Tortiose Two has won!!!");
+    }
+  }, 50);
+}
+
+goButton.addEventListener("click", mouseClick);
